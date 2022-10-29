71f9b39f62119c1e438ff9db70a0ea505c4f101b
gabzintechs <111700863+gabzintechs@users.noreply.github.com>
2022-10-29 15:05:48 -0300

    Add files via upload

---
+++ Poema 01 Ester.py
@@ -0,0 +1,63 @@
+import time
+print("                                    POEMA                  ")
+print("")
+
+time.sleep(1)
+print("""
+No Jardim de meus sentimentos, todas as flores já foram cortadas""")
+
+time.sleep(3)
+print("""
+Entre os Lírios, Gardênias e Margaridas, tudo o que restou são os vestígios de suas trajetórias""")
+
+time.sleep(4)
+print("""
+Ai das flores se soubessem que as plantamos para depois,""")
+
+time.sleep(3)
+print("""cortá-las....
+""")
+
+time.sleep(4)
+print("""Se soubessem, não floreceriam com tamanha maestria""")
+
+time.sleep(3)
+print("""
+E onde estão minhas flores?
+
+Porventura estão ao lado dos túmulos das borboletas que morreram em meu estômago
+""")
+
+time.sleep(5)
+print("""Ainda que o rumo das flores seja catastrófico""")
+
+time.sleep(3)
+print("""Perpetuamente esperarei que o final seja belo.""")
+
+time.sleep(2)
+
+print("")
+
+
+time.sleep(3)
+print(" " * 44, """ ~ Ester Rios
+""")
+time.sleep(3)
+cdt = str(input("deseja ver os creditos ? [S/N]")).upper( )
+
+if cdt == "S" : 
+
+	print("""
+	 - DeV do codigo -
+Luis Gabriel / @075_silvxx
+ """)
+	print("""
+	 - Escritora -
+ Ester Rios / @ester rios
+ """) 
+ 
+if cdt == "N" :
+ 	print("Obrigado por ler !!!")
+
+
+
